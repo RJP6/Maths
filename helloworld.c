409f63de8a2656e3488e814aabc342914e8f45c6
Ryan Payne <p16200083@my365.dmu.ac.uk>
2018-01-09 11:42:07 +0000

    aa

--- helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-   // printf() displays the string inside quotation
-   printf("Hello World!");
-   printf("Fare thee well!");
-   printf("Quicker Version!");
-   return 0;
-}
